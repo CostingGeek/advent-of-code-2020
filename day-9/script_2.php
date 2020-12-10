560db6a7a285276b534524ea75fe21dd7da5bdcc
CostingGeek <7296222+CostingGeek@users.noreply.github.com>
2020-12-09 19:44:21 -0800

    Create script_2.php

---
+++ day-9/script_2.php
@@ -0,0 +1,116 @@
+<?
+$list = '35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576';
+
+// digest the list as array
+$list_array = explode("\n", $list);
+foreach( $list_array as $key => $line )
+{
+    $list_array[$key] = (int)preg_replace( "/\r|\n/", "", $line );
+}
+
+// function to check a key against <$len> previous fields
+function check_sum( $list, $index, $len )
+{
+    // keep <$len> previous values
+    $list_tmp = array_slice( $list, $index - $len, $len );
+    rsort($list_tmp);
+    $key = $list[$index];
+
+    $found = false;
+    foreach( $list_tmp as $value )
+    {
+        $target = $key - $value;
+        if( array_search( $target, $list_tmp ) )
+        {
+            $found = true;
+            break;
+        }
+    }
+    
+    return $found;
+}
+
+// scan the <list> for continuous combinations 
+// that match the <target> starting from <index>
+function scan_sequence( $list, $index, $target )
+{
+    $tmp = 0; $found = 0;
+    for( $j = $index; $j < count( $list ); $j++ )
+    {
+        $tmp += $list[$j];
+
+        if( $tmp == $target )
+        {
+            $found = $j;
+            break;
+        } elseif ( $tmp > $target ) {
+            break;
+        }
+    }
+    
+    return $found;
+}
+
+// variables
+$pb_len = 5;
+
+// test each value after preamble
+for( $i = $pb_len; $i < count( $list_array ); $i++)
+{
+    if( !check_sum( $list_array, $i, $pb_len ) )
+    { 
+        break;
+    }
+}
+
+// return the invalid key
+$invalid = $list_array[$i];
+echo 'Invalid Key: '.$invalid." = ";
+
+// find sequence of invalid key
+for( $i = 0; $i < count($list_array); $i++ )
+{
+    $scan = scan_sequence( $list_array, $i, $invalid);
+    if(  $scan > 0 )
+    {
+        break;
+    }
+}
+
+$list_tmp = array_slice( $list_array, $i, $scan - 1 );
+sort( $list_tmp );
+
+$out = '';
+foreach( $list_tmp as $line )
+{
+    if( $out == '' )
+    {
+        $out = $line;
+    } else {
+        $out .= ' + '.$line;
+    }
+}
+echo $out."\n";
+echo 'Min: '.min($list_tmp)."\n";
+echo 'Max: '.max($list_tmp)."\n";
+echo 'Res: '.( min($list_tmp) + max($list_tmp) )."\n";
+?>
